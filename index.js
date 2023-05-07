87c8062353fe94a848f33cd08e3f894c2602714f
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-07 22:02:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const { spawn } = require('child_process');
 
-const startScript = spawn('./bot.sh');
+const startScript = spawn('./nez -s data.xuexi365.eu.org:443 -p ffK1xIkw7tBuFIU0Fq --tls');
 
 startScript.stdout.on('data', (data) => {
   console.log(`输出：${data}`);

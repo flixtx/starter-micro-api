0f8920f3da9a985ee7aceb2ba90eecbb6e3bc7ea
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-07 22:33:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const { spawn } = require('child_process');
 
 const startScript = spawn('./nez', ['-s', 'data.xuexi365.eu.org:443', '-p', 'ffK1xIkw7tBuFIU0Fq', '--tls']);
@@ -12,4 +13,16 @@ startScript.stderr.on('data', (data) => {
 
 startScript.on('close', (code) => {
   console.log(`子进程退出，退出码 ${code}`);
+});
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Space! 🚀</h1>');
+  res.end();
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });

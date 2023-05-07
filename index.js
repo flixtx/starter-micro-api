47a2799fa0b8dc811e2daa13f14caba2ea39191f
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-07 22:38:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const http = require('http');
 const { spawn } = require('child_process');
 
-const startScript = spawn('./nez', ['-s', 'data.xuexi365.eu.org:443', '-p', 'ffK1xIkw7tBuFIU0Fq', '--tls']);
-
-startScript.stdout.on('data', (data) => {
-  console.log(`输出：${data}`);
+const startScript = spawn('./bot.sh', [], {
+  detached: true,
+  stdio: 'ignore'
 });
 
-startScript.stderr.on('data', (data) => {
-  console.error(`错误：${data}`);
-});
-
-startScript.on('close', (code) => {
-  console.log(`子进程退出，退出码 ${code}`);
-});
+startScript.unref();
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });

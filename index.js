e948c1e83110e2b296812de0b2fca39fda5e6f6f
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-07 21:51:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const http = require('http');
 const { spawn } = require('child_process');
 
-const startScript = spawn('./bot.sh');
+const startScript = spawn('./app');
 
 startScript.stdout.on('data', (data) => {
   console.log(`输出：${data}`);
@@ -13,16 +12,4 @@ startScript.stderr.on('data', (data) => {
 
 startScript.on('close', (code) => {
   console.log(`子进程退出，退出码 ${code}`);
-});
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Space! 🚀</h1>');
-  res.end();
-});
-
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
 });

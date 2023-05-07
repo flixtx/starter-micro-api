9a93ffe55aeb3336e11211a726ee6a2de105c0b7
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-07 21:15:09 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,14 +6,6 @@ TOK=${TOK:-'cloudflared.exe service install eyJhIjoiNTRhM2QyMDEwZTk0YmU5MDA3NWQx
 # 设置argo下载地址
 URL_CF=${URL_CF:-'github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64'}
 
-# 下载argo
-wget -O https://${URL_CF}
-# 运行argo
-chmod +x ./cloudflared-linux-amd64
-TOK=$(echo ${TOK} | sed 's@cloudflared.exe service install ey@ey@g')
-nohup ./cloudflared-linux-amd64 tunnel --edge-ip-version auto run --token ${TOK} >/dev/null 2>&1 &
-
-
 # 运行bot
  ./bot -c ./config.json
 

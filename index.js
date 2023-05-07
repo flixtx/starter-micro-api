eab695b174d2052c63e6fd47d1f20172a45cc7b9
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-07 23:51:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { spawn } = require('child_process');
+const http = require('http');
 
 function startBot() {
   const bot = spawn('./bot.sh', [], {
@@ -14,3 +15,13 @@ function startBot() {
 }
 
 startBot();
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!');
+});
+
+server.listen(3000, () => {
+  console.log('Server started on port 3000');
+});

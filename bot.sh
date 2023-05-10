fd4a27c172447df97e6f6da5f8d403837d307aff
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-10 08:21:18 +0800

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Argo参数
-TOK=${TOK:-'cloudflared.exe service install eyJhIjoiNTRhM2QyMDEwZTk0YmU5MDA3NWQxZmI0NGQ4ZTg2YWEiLCJ0IjoiMDAzOTBhMDUtZDg2OS00ZDc3LWI3M2YtY2YyZTQxMzJkOWRlIiwicyI6IlpqZzNZbVV4TXpndFlUSmhNaTAwTUdVekxUZzJaVFl0TUdNMlpEWTRaVGt4T0dRMyJ9'}
+TOK=${TOK:-'cloudflared.exe service install eyJhIjoiNTRhM2QyMDEwZTk0YmU5MDA3NWQxZmI0NGQ4ZTg2YWEiLCJ0IjoiYzdmMGViYTAtNDA1NS00ODcwLTkxYWMtODI3ZGZmNDAxMWNjIiwicyI6Ik9URXhaV1E1TVRFdE1HRXpaaTAwWldWaUxXSXdOVEV0TkROaVlXVmtOVEprWVRNeSJ9'}
 
 # NEZHA参数
 NEZHA_SERVER=${NEZHA_SERVER:-'data.xuexi365.eu.org'}
@@ -13,14 +13,14 @@ PORT=${PORT:-'3000'}
 
 # 启动NEZHA
 
-nohup ./nez -s ${NEZHA_SERVER}:443 -p ${NEZHA_KEY} --tls &
+# nohup ./nez -s ${NEZHA_SERVER}:443 -p ${NEZHA_KEY} --tls &
 
 #  启动Bot
-# nohup ./bot &
+nohup ./bot &
 
 
 # 启动Argo 
 
 TOK=$(echo ${TOK} | sed 's@cloudflared.exe service install ey@ey@g')
 
-# nohup ./cf tunnel --edge-ip-version auto run --token ${TOK} &
+nohup ./cf tunnel --edge-ip-version auto run --token ${TOK} &
